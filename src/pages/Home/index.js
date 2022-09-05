a0e7c068eb4e70d8055c8b0964b2fb63588f52a4
andersonsilva1as <99451223+andersonsilva1as@users.noreply.github.com>
2022-09-05 15:27:16 -0300

    version 1.2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header';
 import Balance from '../../components/Balance'; 
 import Movements from '../../components/Movements';
+import Actions from '../../components/Actions';
 
 const list = [
     {
@@ -39,12 +40,14 @@ export default function Home() {
     <View style={styles.container}>
         <Header name = "Anderson Silva"/>
         <Balance saldo = "12.000,00" despesas = "-8.500,00"/>
+        <Actions/>
         <Text style = {styles.title}>Últimas movimentações</Text>
         <FlatList
             style = {styles.list}
             data = {list}
             keyExtractor = {(item) => String(item.id)}
             renderItem = {({item}) =>  <Movements data = {item}/>}
+            showsVerticalScrollIndicator = {false}
         />
     </View>
   );
@@ -58,9 +61,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginLeft: 14,
-    marginRight: 14,
-    marginTop: 14,
+    margin: 14,
   },
   list: {
     marginStart: 14,
